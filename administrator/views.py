2cdda3e0e490674f85697cf7c05ef9d2372c5f25
Luxim0rd <blackmetal211@gmail.com>
2020-01-27 16:12:41 -0500

    login

---
+++ administrator/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.contrib.auth import logout as do_logout
+
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as do_login
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+from django.shortcuts import render, redirect
+
+def welcome(request):
+    # Si estamos identificados devolvemos la portada
+    if request.user.is_authenticated:
+        return render(request, "welcome.html")
+    # En otro caso redireccionamos al login
+    return redirect('/login')
+
+
+
+
+
+def login(request):
+    # Creamos el formulario de autenticación vacío
+    form = AuthenticationForm()
+    if request.method == "POST":
+        # Añadimos los datos recibidos al formulario
+        form = AuthenticationForm(data=request.POST)
+        # Si el formulario es válido:
+        if form.is_valid():
+            # Recuperamos las credenciales validadas
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            # Verificamos las credenciales del usuario
+            user = authenticate(username=username, password=password)
+
+            # Si existe un usuario con ese nombre y contraseña
+            if user is not None:
+                # Hacemos el login manualmente
+                do_login(request, user)
+                # Y le redireccionamos a la portada
+                return redirect('/')
+
+    # Si llegamos al final renderizamos el formulario
+    return render(request, "login.html", {'form': form})
+
+
+def logout(request):
+    # Finalizamos la sesión
+    do_logout(request)
+    # Redireccionamos a la portada
+    return redirect('/')
